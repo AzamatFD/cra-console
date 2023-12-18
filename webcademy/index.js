eefc7333cc1f7977359b7f90b2a5730c2c3e2832
Azamat F <farrakhovazamatd@gmail.com>
2023-12-18 13:24:10 +0500

    first commit

---
+++ webcademy/index.js
@@ -0,0 +1,2 @@
+let userName = 'Aza'
+conxole.log(userName)
---
+++ webcademy/main.js
@@ -0,0 +1,8 @@
+const autoBrands = ['audi', 'lada', 'suzuki']
+
+// for(let item of autoBrands){
+//   alert(item)
+// }
+autoBrands.forEach(function(item){
+  console.log(item)
+})
